a41b4ed9e0c2fc880ac3c07e3f66a94a62926336
Raycer <Raycer@users.noreply.github.com>
2016-02-18 00:25:00 +0100

    Update install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -5,7 +5,7 @@ if [ ! -d $doti3 ]; then
     mkdir $doti3
 fi
 
-dotfiles_install_package i3 i3status dmenu rofi-git i3-gaps
+dotfiles_install_package i3 i3status dmenu rofi i3-gaps
 
 dotfiles_install_component i3config $doti3/config
 dotfiles_install_component i3status.conf $doti3/i3status.conf
