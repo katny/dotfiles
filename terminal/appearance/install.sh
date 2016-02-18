9635998e796fae6dfd73df75617d746ccc14b217
Raycer <Raycer@users.noreply.github.com>
2016-02-18 11:29:14 +0100

    Update install.sh

--- terminal/appearance/install.sh
+++ terminal/appearance/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-dotfiles_install_remote_component GITHUB tlatsas/xcolors xcolors
 dotfiles_install_component .Xresources $HOME/.Xresources
 
-# Update X configuration
-xrdb -I$DOTFILES_CURRENT_SOURCE_DIR/xcolors/themes -merge $HOME/.Xresources
+
