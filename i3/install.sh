5c8b3360aa62facd3cc9a2835b41a718eaeedfc4
Raycer <Raycer@users.noreply.github.com>
2016-02-19 00:11:21 +0100

    Update install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -5,13 +5,9 @@ fonts=$HOME/.fonts
 if [ ! -d $doti3 ]; then
     mkdir $doti3
 fi
-if [ ! -d $fonts ]; then
-    mkdir $fonts
-fi
 
 dotfiles_install_package i3 i3status dmenu rofi i3-gaps
 
 dotfiles_install_component i3config $doti3/config
 dotfiles_install_component i3status.conf $doti3/i3status.conf
 dotfiles_install_component conky.conf $doti3/conky.conf
-dotfiles_install_remote_component GITHUB supermarin/YosemiteSanFranciscoFont $fonts
