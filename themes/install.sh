a89cdd2727e9ed2312ca55c63867a368a8de65d6
Raycer <Raycer@users.noreply.github.com>
2016-02-19 02:34:32 +0100

    Update install.sh

--- themes/install.sh
+++ themes/install.sh
@@ -1,9 +1,14 @@
 
 themes=$HOME/.themes
+icons=$HOME/.icons
 
 if [ ! -d $themes ] then
     mkdir $themes
 fi
+if [ ! -d $icons ] then
+    mkdir $icons
+fi
 
 dotfiles_install_component .gtkrc-2.0 $HOME/
 dotfiles_install_component diehard4 $themes/
+dotfiles_install_component Treepata $icons/
