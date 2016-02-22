b3c36e7fadf0c0927acb52c0ef3a1eeebd2cc60e
manjaro <manjaro@localhost.localdomain>
2016-02-22 02:05:43 +0100

    modified:   .dotfileslib/.package-database wq	modified:   themes/install.sh

--- themes/install.sh
+++ themes/install.sh
@@ -30,9 +30,5 @@ dotfiles_install_component .gtkrc-2.0 $HOME/.gtkrc-2.0
 dotfiles_install_component .xinitrc $HOME/.xinitrc
 dotfiles_install_component diehard4 $themes/diehard4
 dotfiles_install_component Treepata $icons/Treepata
-dotfiles_install_component wallpapers/CyberParis.jpg $pixmaps/wallpapers/CyberParis.jpg
-dotfiles_install_component wallpapers/cyberEye.jpg $pixmaps/wallpapers/cyberEye.jpg 
-dotfiles_install_component wallpapers/greenNinja.png $pixmaps/wallpapers/greenNinja.png
-dotfiles_install_component wallpapers/tiger.png $pixmaps/wallpapers/tiger.png
 dotfiles_install_component lightdm-gtk-greeter.conf $lightDM/lightdm-gtk-greeter.conf
 dotfiles_install_component bg-saved.cfg $nitrogen/bg-saved.cfg
