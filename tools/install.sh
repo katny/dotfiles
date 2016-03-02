76730475c8e9663d420e7dea33f274d69b7187db
Raycer <Raycer@users.noreply.github.com>
2016-03-02 02:26:55 +0100

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -2,9 +2,10 @@
 ###################################################################################################################################
 ###This simple bash script will work in arch based systems only, and it will try to install everything in the packages_list.txt#### 
 ###################################################################################################################################
+PACKAGES="${DOTFILES_CURRENT_SOURCE_DIR}/package_list.txt"
 
 pacman -Qe | awk '{print $1}' > current_list.txt
-diff current_list.txt package_list.txt | grep ">" | sed 's/> //g' > diff_list.txt
+diff current_list.txt  $PACKAGES | grep ">" | sed 's/> //g' > diff_list.txt
 rm current_list.txt
 
 for x in $(cat diff_list.txt)
