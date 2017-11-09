107c94b7e40753dab770897f47afc2d174ffe22c
Pastitas <Pastitas@users.noreply.github.com>
2017-11-09 12:10:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ dotfiles_install()
 {
     dotfiles_install_target terminal/zsh
     dotfiles_install_target terminal/appearance
-    dotfiles_install_target editors/vim
+   # dotfiles_install_target editors/vim
     dotfiles_install_target editors/patata
     dotfiles_install_target i3
     dotfiles_install_target themes
