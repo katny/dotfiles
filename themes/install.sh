12479835d35a96c217c7b21f555f616b035fb8dd
pastitas <rootwow@gmail.com>
2017-11-30 03:53:49 +0100

    modified:   themes/.gtkrc-2.0 	deleted:    themes/Boje-Blue/cinnamon/add-workspace-button-hover.svg 	deleted:    themes/Boje-Blue/gtk-2.0/Arrows/arrow-up-prelight.png

--- themes/install.sh
+++ themes/install.sh
@@ -1,8 +1,4 @@
-
-
-themes=$HOME/.themes
-icons=$HOME/.icons
-pixmaps=/usr/share/pixmaps
+backgrounds=/usr/share/backgrounds
 lightDM=/etc/lightdm
 config=$HOME/.config
 nitrogen=$HOME/.config/nitrogen
@@ -10,12 +6,6 @@ wallpapers=$HOME/.dotfiles/themes/wallpapers
 dotThemes=$HOME/.dotfiles/themes
 
 
-if [ ! -d $themes ]; then
-    mkdir $themes
-fi
-if [ ! -d $icons ]; then
-    mkdir $icons
-fi
 if [ ! -d $nitrogen ]; then
     mkdir $nitrogen
 fi
@@ -23,14 +13,14 @@ if [ ! -d $config ]; then
     mkdir $config
 fi
 
-sudo cp -r  $wallpapers $pixmaps/
-sudo cp  $dotThemes/lightdm-gtk-greeter.conf $lightdm/
+sudo cp -r  $wallpapers/ $backgrounds
+sudo cp -r $dotThemes/lightdm-gtk-greeter.conf $lightdm/
 
 
-dotfiles_install_package nitrogen lightdm numix-circle
+dotfiles_install_package nitrogen lightdm 
+yaourt numix-circle 
+yaourt arc-gtk
 
 dotfiles_install_component .gtkrc-2.0 $HOME/.gtkrc-2.0
 dotfiles_install_component .xinitrc $HOME/.xinitrc
-dotfiles_install_component diehard4 $themes/diehard4
-dotfiles_install_component Treepata $icons/Treepata
 dotfiles_install_component bg-saved.cfg $nitrogen/bg-saved.cfg
